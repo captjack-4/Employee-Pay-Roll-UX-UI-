7aaa839709e60544d2291bb1e8d010cbd86680cc
unknown <akshayHVshinde04@gmail.com>
2022-01-07 18:07:09 +0530

    Employee Data

---
+++ EmpPayRollApp/JavaScript/EmpData.js
@@ -0,0 +1,38 @@
+class employeepayrollData {
+    
+    get id() { return this._id}
+    set id(id) {  
+        this._id = id
+    }
+
+    get name() { return this._name}
+    set name(name) {
+        this._name = name;
+    }
+
+    get profilePic() {return this._profilePic}
+    set profilePic(profilePic) {
+        this._profilePic = profilePic
+    }
+
+    get gender() {return this._gender}
+    set gender(gender) {
+        this._gender = gender
+    }
+
+    get department() { return this._department}
+    set department(department) {
+        this._department = department
+    }
+    
+    get salary() {return this._salary}
+    set salary(salary) {
+        this._salary = salary
+    }
+
+    get note() {return this._note}
+    set note(note) {
+        this._note = note
+    }
+    
+}
